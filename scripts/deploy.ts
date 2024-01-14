fdf34af57c50e28e8aafbe866428c7dfcf16d39e
Nawodya Ishan <nawodyain@gmail.com>
2024-01-14 22:58:52 +0530

    feat: update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,30 @@
-import {ethers} from "hardhat";
+import {ethers, network} from "hardhat";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const string = "Hi 2024!!";
+/**
+ * Main async function to deploy the SimpleStorage contract.
+ */
+const main = async () => {
+    // Creating a contract factory for the SimpleStorage contract.
+    // This factory is an abstraction used to deploy new smart contracts.
+    const simpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
 
-  const lockedAmount = ethers.parseEther("0.001");
+    // Deploying the SimpleStorage contract.
+    // This operation is asynchronous and returns a Promise.
+    const simpleStorageContractInstance = await simpleStorageFactory.deploy();
 
-  const lock = await ethers.deployContract("SimpleStorage", [string], {
-    value: lockedAmount,
-  });
+    // Wait for the contract deployment to be confirmed on the network.
+    // This ensures the deployment process is complete before proceeding.
+    await simpleStorageContractInstance.waitForDeployment();
 
-  await lock.waitForDeployment();
+    // Logging the address to which the SimpleStorage contract was deployed.
+    // This address is used to interact with the contract in the future.
+    console.log(
+        `SimpleStorage deployed to ${await simpleStorageContractInstance.getAddress()} on network: ${network.name}`
+    );
+};
 
-  console.log(
-      `Lock with ${ethers.formatEther(
-          lockedAmount,
-      )}ETH and unlock timestamp ${string} deployed to ${lock.target}`,
-  );
-}
-
-// We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error("Error encountered during contract deployment:", error);
+    process.exitCode = 1;
 });
