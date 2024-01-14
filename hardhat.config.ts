73f08b86328066fb2fe182102b3418e0c13e1402
Nawodya Ishan <nawodyain@gmail.com>
2024-01-15 01:04:15 +0530

    feat: add EnvUtils

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,24 +1,24 @@
 import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import dotenv from "dotenv";
+import {EnvUtils} from "./utils/envUtils";
 
 dotenv.config();
 
+// Check required environment variables
+EnvUtils.checkEnvVariables([
+    "INFURA_API_KEY",
+    "SEPOLIA_RPC_URL",
+    "AVALANCHE_FUJI_RPC_URL",
+    "PRIVATE_KEY"
+]);
+
 // Accessing Environment variables
-const INFURA_API_KEY = process.env.INFURA_API_KEY
-if (!INFURA_API_KEY) throw new Error("Environment variable INFURA_API_KEY is not set!")
-
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
-if (!SEPOLIA_RPC_URL) throw new Error("Environment variable SEPOLIA_RPC_URL is not set!")
-
-const AVALANCHE_FUJI_RPC_URL = process.env.AVALANCHE_FUJI_RPC_URL
-if (!AVALANCHE_FUJI_RPC_URL) throw new Error("Environment variable AVALANCHE_FUJI_RPC_URL is not set!")
-
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-if (!PRIVATE_KEY) throw new Error("Environment variable PRIVATE_KEY is not set!")
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL!;
+const PRIVATE_KEY = process.env.PRIVATE_KEY!;
 
 const config: HardhatUserConfig = {
-    solidity: "0.8.19",
+    solidity: "0.8.20",
     networks: {
         hardhat: {},
         sepolia: {
